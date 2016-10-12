51c5df2c6be1f6557ea28ba0eae1b3236804308f
cjredmond <connor.redmond@gmail.com>
2016-10-11 23:03:52 -0400

    final commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def equation(a,b,c):
+    if c == "+":
+        return int(a) + int(b)
+    elif c == "-":
+        return int(a) - int(b)
+    elif c == "x":
+        return int(a) * int(b)
+    elif c == "/":
+        if b == "0":
+            return "Cant divide by 0"
+        return int(a) / int(b)
+    else:
+        return "HELP"
+
+def stringify(a,b,c):
+    return a + b + c +" ="
+
+
+def index_view(request):
+    numbers = request.GET
+    if request.GET:
+        number1 = numbers["number1"]
+        number2 = numbers["number2"]
+        operation = numbers["operation"]
+    else:
+        number1 = 1
+        number2 = 1
+        operation = "+"
+
+    context = {
+    "answer" : equation(number1, number2, operation),
+    "EQ" : stringify(number1, operation, number2)
+    }
+    return render(request, "index.html", context)
